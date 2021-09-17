4c1d145855d66675de65650e16f118d57bf84292
Reza <r.bahar91@gmail.com>
2021-09-17 06:33:43 -0700

    Update: disabled og:image, fix later

--- components/meta.js
+++ components/meta.js
@@ -35,11 +35,11 @@ export default function Meta() {
         name="description"
         content={"TODO, Reza Baharvand | Web developer, JavaScript, etc."} // TODO
       />
-      <meta
+      {/* <meta
         property="og:image"
         content={TODO}
-        // TODO, create an image like this https://og-image.vercel.app/Next.js%20Blog%20Example%20with%20**Strapi**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Fv1590740734%2Fnextjs%2Fexamples%2Fstrapi-logo.svg
-      />
+        TODO, create an image like this https://og-image.vercel.app/Next.js%20Blog%20Example%20with%20**Strapi**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Fv1590740734%2Fnextjs%2Fexamples%2Fstrapi-logo.svg
+      /> */}
     </Head>
   );
 }
