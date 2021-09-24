13fe3dd34432b742415147136bb7faa1dcd0202c
Reza <r.bahar91@gmail.com>
2021-09-24 00:49:47 -0700

    update: author and date

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -1,4 +1,4 @@
-import Avatar from "../components/Avatar";
+import Author from "../components/Author";
 import Date from "../components/Date";
 import PostTitle from "../components/PostTitle";
 import CoverImage from "../components/CoverImage";
@@ -7,16 +7,14 @@ export default function PostHeader({ title, date, author, coverImage }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
+      <div className="flex items-center mb-3">
+        <div className="text-sm text-gray-500 mr-1">
+          <Date dateString={date} /> by
+        </div>
+        <Author name={author.name} />
+      </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} url={coverImage.url} />
-      </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="mb-3">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-12 text-sm text-gray-500">
-          <Date dateString={date} />
-        </div>
       </div>
     </>
   );
