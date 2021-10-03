09a8dcd9125db6880e0a43f18c885d4cfc1652ad
Reza <r.bahar91@gmail.com>
2021-10-03 04:43:39 -0700

    feat: updated time for posts

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -3,15 +3,24 @@ import Date from "../components/Date";
 import PostTitle from "../components/PostTitle";
 import CoverImage from "../components/CoverImage";
 
-export default function PostHeader({ title, date, author, coverImage }) {
+export default function PostHeader({
+  title,
+  date,
+  author,
+  coverImage,
+  updatedTime,
+}) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="flex items-center mb-3">
+      <div className="flex items-center mb-1">
         <div className="text-sm text-gray-500 mr-1">
-          <Date dateString={date} /> by
+          Published on <Date dateString={date} /> | by
         </div>
         <Author name={author.name} />
+      </div>
+      <div className="text-sm text-gray-500 mr-1 mb-3">
+        Updated on <Date dateString={updatedTime} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} url={coverImage.url} />
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -36,6 +36,7 @@ export default function Post({ post, morePosts, preview }) {
                 coverImage={post.coverImage}
                 date={post.date}
                 author={post.author}
+                updatedTime={post.updated_at}
               />
               <PostBody content={post.content} />
             </article>
