6798c29078f6d5b8523343ad911e75cfc47cb55e
Reza <r.bahar91@gmail.com>
2021-10-08 10:02:23 -0700

    add: blog archive  meta tags

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -11,9 +11,49 @@ export default function Home({ allPosts, preview }) {
       <Layout>
         <Head>
           <title>
-            Reza Baharvand | Web developer, Reactjs, Nextjs, Nodejs, MongoDB,
-            PostgreSQL, Expressjs
+            Reza Baharvand&apos;s Blog | Web developer, Reactjs, Nextjs, Nodejs,
+            MongoDB, PostgreSQL, Expressjs
           </title>
+          <meta
+            name="title"
+            content="Reza Baharvand's blog | Full-stack Developer"
+          />
+          <meta
+            name="description"
+            content="Reza Baharvand's blog | Web developer, Reactjs, Nextjs, Nodejs, MongoDB, PostgreSQL, GraphQL, Expressjs, GSAP, D3js"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://rezabaharvand.dev/blog" />
+          <meta
+            property="og:title"
+            content="Reza Baharvand's blog | Full-stack Developer"
+          />
+          <meta
+            property="og:description"
+            content="Reza Baharvand's blog | Web developer, Reactjs, Nextjs, Nodejs, MongoDB, PostgreSQL, GraphQL, Expressjs, GSAP, D3js"
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/rezathematic/image/upload/v1633711435/Reza_Baharvand_profile_picture_46edd8fe9d.jpg"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://rezabaharvand.dev/blog"
+          />
+          <meta
+            property="twitter:title"
+            content="Reza Baharvand's blog | Full-stack Developer"
+          />
+          <meta
+            property="twitter:description"
+            content="Reza Baharvand's blog | Web developer, Reactjs, Nextjs, Nodejs, MongoDB, PostgreSQL, GraphQL, Expressjs, GSAP, D3js"
+          />
+          <meta
+            property="twitter:image"
+            content="https://res.cloudinary.com/rezathematic/image/upload/v1633711435/Reza_Baharvand_profile_picture_46edd8fe9d.jpg"
+          />
         </Head>
         <Container>
           <RecentPosts posts={allPosts} />
